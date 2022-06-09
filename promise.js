37f375a8e7034ac7cb1ec6c032ceffb332f26ecc
zulham.sy@gmail.com <zulham.sy@gmail.com>
2022-06-09 21:35:39 +0700

    test promise #1

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async function(param) {
+	let result = 0;
+	const IXX = await promiseTheaterIXX()
+	const VGC = await promiseTheaterVGC()
+	const TABLE = IXX.concat(VGC)
+	TABLE.forEach((data) => {
+		if(data.hasil == param) result++
+	})
+	
+return result
+};
 
 module.exports = {
   promiseOutput,
